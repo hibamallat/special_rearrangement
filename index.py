225d352b30b67cff24fd036585992e8e6c8579f9
hibamallat <hibamallat040@gmail.com>
2024-11-05 05:39:43 +0200

    Allow the user to fill the list

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-numbers_list = [2,3,1,4,5]
+numbers_list = []
 even_num = []
 odd_num = []
 
@@ -8,8 +8,16 @@ def special_rearrangement(nums):
             even_num.append(nums[i])
         else:
             odd_num.append(nums[i])
+
     rearrange_list = even_num + odd_num
     return rearrange_list
+
+for i in range (8):
+    num = int(input("Please enter a number to include it in the list: "))
+    numbers_list.append(num)
+    if num == 0:
+        numbers_list.remove(num)
+        break
 
 print("Numbers list:", numbers_list)
 
