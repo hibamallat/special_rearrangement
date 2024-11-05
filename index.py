ea326ea1bb32738d74c050e0e0ec1bca59e242e7
hibamallat <hibamallat040@gmail.com>
2024-11-05 05:25:40 +0200

    little change in loop

--- index.py
+++ index.py
@@ -3,13 +3,14 @@ even_num = []
 odd_num = []
 
 def special_rearrangement(nums):
-    for i in nums:
-        if i % 2 == 0:
-            even_num.append(i)
+    for i in range (len(nums)):
+        if nums[i] % 2 == 0:
+            even_num.append(nums[i])
         else:
-            odd_num.append(i)
+            odd_num.append(nums[i])
     rearrange_list = even_num + odd_num
     return rearrange_list
+
 print("Numbers list:", numbers_list)
-list = special_rearrangement(numbers_list)
-print("Rearrange list:", list)
+
+print("Rearrange list:", special_rearrangement(numbers_list))
