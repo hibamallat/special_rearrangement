55df4cdd5836944db2626ff1d5451e5fc1580ff6
hibamallat <hibamallat040@gmail.com>
2024-11-05 05:45:48 +0200

    Validate the input

--- index.py
+++ index.py
@@ -11,14 +11,16 @@ def special_rearrangement(nums):
 
     rearrange_list = even_num + odd_num
     return rearrange_list
+try:
+    for i in range (8):
+        num = int(input("Please enter a number to include it in the list: "))
+        numbers_list.append(num)
+        if num == 0:
+            numbers_list.remove(num)
+            break
 
-for i in range (8):
-    num = int(input("Please enter a number to include it in the list: "))
-    numbers_list.append(num)
-    if num == 0:
-        numbers_list.remove(num)
-        break
+    print("Numbers list:", numbers_list)
 
-print("Numbers list:", numbers_list)
-
-print("Rearrange list:", special_rearrangement(numbers_list))
+    print("Rearrange list:", special_rearrangement(numbers_list))
+except ValueError:
+    print("INVALID: Enter a number")
